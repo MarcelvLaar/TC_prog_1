2d3474f5f47003d570db1bf09d9effbd52702977
Marcel van laar <m.s.laar.v@gmail.com>
2023-12-01 09:40:49 +0100

    wat werk aan ex7

--- src/Calendar.hs
+++ src/Calendar.hs
@@ -36,18 +36,21 @@ scanCalendar =  greedy scanLine
 isString :: Parser Char String
 isString = greedy isStrOrSp
 
-isStrOrSp :: Parser Char Char
-isStrOrSp = satisfy isAlphaNum <|> symbol ' '
+isStrOrSp :: Parser Char Char --dit moet beter kunnen, toch? misschien anySymbol?
+isStrOrSp = satisfy isAlphaNum <|> symbol ' ' <|> symbol '.' <|> symbol '/' <|> symbol '-' <|> symbol '@'
 
 scanTest :: Parser Char Token
-scanTest = Token <$> token "1" <*> token "2" 
+scanTest = Token <$> token "1" <*> token "2"
 
 scanLine :: Parser Char Token
-scanLine =  (\a b -> (Token a b)) <$> isString <* symbol ':' <*> isString <* symbol '\n' <<|>
-            (\a b -> (Token a b)) <$> isString <* symbol ':' <*> isString <* symbol '\n' <<|>
-            (\a b -> (Token a b)) <$> isString <* symbol ':' <*> isString
+-- scanLine =  Token <$> isString <* symbol ':' <*> isString <* symbol '\n' <<|>
+--             Token <$> isString <* symbol ':' <*> isString <* symbol '\n' <<|>
+--             Token <$> isString <* symbol ':' <*> isString
+scanLine =  Token <$> isString <* symbol ':' <*> isString <* symbol '\n' <<|>
+            Token <$> isString <* symbol ':' <*> isString 
 
-testsl = parse scanCalendar "Testing:12315245\ntest2:testing\ndoesit:work\nihope:so\nhmm: I hope it works"
+sl_test_1 = run scanCalendar "Testing:12315245\ntest2:testing\ndoesit:work\nihope:so\nhmm: I hope it works"
+sl_test_2 = run scanCalendar "BEGIN:VCALENDAR\n PRODID:-//hacksw/handcal//NONSGML v1.0//EN\n VERSION:2.0\n BEGIN:VEVENT\n SUMMARY:Bastille Day Party\n UID:19970610T172345Z-AF23B2@example.com\n DTSTAMP:19970610T172345Z\n DTSTART:19970714T170000Z\n DTEND:19970715T040000Z\n END:VEVENT\n END:VCALENDAR"
 
 parseCalendar :: Parser Token Calendar
 parseCalendar = undefined
