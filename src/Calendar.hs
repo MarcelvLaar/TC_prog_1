1b65d1097f93c065f496a8583b69e05420cb1dc1
Marcel van laar <m.s.laar.v@gmail.com>
2023-12-01 20:44:14 +0100

    fixed descriptions over multiple lines

--- src/Calendar.hs
+++ src/Calendar.hs
@@ -39,21 +39,23 @@ isString = greedy isStrOrSp
 isStrOrSp :: Parser Char Char
 isStrOrSp = satisfy (\c -> c /= '\n' && c /= ':')
 
-scanTest :: Parser Char Token
-scanTest = Token <$> token "1" <*> token "2"
+newLineAllowed :: Parser Char String
+newLineAllowed = greedy (satisfy (/= ':'))
 
 scanLine :: Parser Char Token
--- scanLine =  Token <$> isString <* symbol ':' <*> isString <* symbol '\n' <<|>
---             Token <$> isString <* symbol ':' <*> isString <* symbol '\n' <<|>
---             Token <$> isString <* symbol ':' <*> isString
 scanLine =  Token <$> isString <* symbol ':' <*> isString <* symbol '\n' <<|>
-            Token <$> isString <* symbol ':' <*> isString
+            Token <$> isString <* symbol ':' <*> isString <<|>
+            Token <$> newLineAllowed <* symbol ':' <*> isString <* symbol '\n'
 
 sl_test_1 = run scanCalendar "Testing:12315245\ntest2:testing\ndoesit:work\nihope:so\nhmm: I hope it works"
 sl_test_2 = run scanCalendar "BEGIN:VCALENDAR\n PRODID:-//hacksw/handcal//NONSGML v1.0//EN\n VERSION:2.0\n BEGIN:VEVENT\n SUMMARY:Bastille Day Party\n UID:19970610T172345Z-AF23B2@example.com\n DTSTAMP:19970610T172345Z\n DTSTART:19970714T170000Z\n DTEND:19970715T040000Z\n END:VEVENT\n END:VCALENDAR"
+sl_test_3 = run scanCalendar "BEGIN:VCALENDAR\n PRODID:-//hacksw/handcal//NONSGML v1.0//EN\n VERSION:2.0\n BEGIN:VEVENT\n SUMMARY:Bastille \nDay\n Party\n UID:19970610T172345Z-AF23B2@example.com\n DTSTAMP:19970610T172345Z\n DTSTART:19970714T170000Z\n DTEND:19970715T040000Z\n END:VEVENT\n END:VCALENDAR"
 
 parseCalendar :: Parser Token Calendar
 parseCalendar = undefined
+
+parenthesised :: Parser Char a -> Parser Char a
+parenthesised p = pack (symbol '(') p (symbol ')')
 
 recognizeCalendar :: String -> Maybe Calendar
 recognizeCalendar s = run scanCalendar s >>= run parseCalendar
